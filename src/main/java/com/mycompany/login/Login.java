504a75ca93a3e47e0e1a37b1eea96dc86dbfa377
siyab <siyab@LAPTOP-GB2KUB0B>
2024-09-13 16:35:46 +0200

    Second Commit

--- src/main/java/com/mycompany/login/Login.java
+++ src/main/java/com/mycompany/login/Login.java
@@ -13,6 +13,11 @@ public class Login {
     private String password;
     private String firstName;
     private String lastName;
+    
+    // Method to check if username is valid or not
+    public boolean checkUserName(String username) {
+        return username.contains("_") && username.length() <= 5;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
