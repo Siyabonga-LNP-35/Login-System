f056955a0cbcf779e3070eb16266a3ad9ca887ad
siyab <siyab@LAPTOP-GB2KUB0B>
2024-09-13 19:30:38 +0200

    9th Commit

---
+++ src/main/java/main.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author siyab
+ */
+  public static void main(String[] args) {
+        
+       Scanner sc=new Scanner (System.in);
+       
+        System.out.println("Enter name : ");
+        String name = sc.next();
+        
+        System.out.println("lastname : ");
+        String lastname = sc.next();
+        
+        System.out.println("Enter Date of Birth : ");
+        String  dateofbirth= sc.next();
+        
+        System.out.println("Enter Your Gender: ");
+        String gender = sc.next();
+        
+        System.out.println("Enter Your age: ");
+        String age = sc.next();
+
+    } 
+
