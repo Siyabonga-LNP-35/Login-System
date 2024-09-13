d5c5c61fd68949f3aa18e64b249e7156f817c831
siyab <siyab@LAPTOP-GB2KUB0B>
2024-09-13 16:33:26 +0200

    First Commit

--- src/main/java/com/mycompany/login/Login.java
+++ src/main/java/com/mycompany/login/Login.java
@@ -9,6 +9,10 @@ package com.mycompany.login;
  * @author siyab
  */
 public class Login {
+     private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
